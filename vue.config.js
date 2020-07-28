7601877c44421730bd54cfb4804e279eb63de418
jiwei.li <jiwei.li@ly.com>
2020-07-28 17:53:17 +0800

    ...

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,33 @@
 'use strict';
 const path = require('path');
 const fs = require('fs-extra');
+const autodllWebpackPlugin = require("autodll-webpack-plugin")
 function resolve(dir) {
 	return path.join(__dirname, dir);
 }
 
 module.exports = {
-	
+	publicPath: "./",
 	chainWebpack(config) {
 		config
 			.plugin('html')
 			.tap(args => {
-                console.log("------------------")
-                console.log(args)
 				return args
-			})
+            });
+        config
+            .plugin("dll")
+            .use(autodllWebpackPlugin, [{
+                inject: true,
+                debug: true,
+                filename: '[name]_[contenthash].js',
+                path: './dll',
+                entry: {
+                    vendor: [
+                        'vue',
+                        'vue-router'
+                    ]
+                }
+            }]);
+        return config
 	}
 };
