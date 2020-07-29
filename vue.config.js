a7e2d502066e9502dc2f41bcae726a68d42dbd91
lijiwei <1101407798@qq.com>
2020-07-29 09:17:56 +0800

    ..

--- vue.config.js
+++ vue.config.js
@@ -1,38 +1,56 @@
 'use strict';
 const path = require('path');
 const fs = require('fs-extra');
-const autodllWebpackPlugin = require("autodll-webpack-plugin")
+// const autodllWebpackPlugin = require("autodll-webpack-plugin")
+const optimizeCss = require("optimize-css-assets-webpack-plugin")
 function resolve(dir) {
 	return path.join(__dirname, dir);
 }
 
 module.exports = {
-	publicPath: "./",
+    publicPath: "./",
+    css: {
+        extract: {
+            publicPath: "//a.b.com/"
+        }
+    },
 	chainWebpack(config) {
         config  
             .entryPoints
                 .clear();
         config.entry("main/app").add("./src/main.js");
+        // config
+        //     .externals({
+        //         "vue": "Vue",
+        //         "vue-router": "VueRouter"
+        //     })
 		config
 			.plugin('html')
 			.tap(args => {
 				return args
             });
+        // config
+        //     .plugin("dll")
+        //     .use(autodllWebpackPlugin, [{
+        //         inject: true,
+        //         debug: true,
+        //         filename: '[name]_[contenthash].js',
+        //         path: './dll',
+        //         entry: {
+        //             vendor: [
+        //                 'vue',
+        //                 'vue-router',
+        //                 'core-js'
+        //             ]
+        //         }
+        //     }]);
+        // config
+        //     .plugin("optimize-css")
+        //         .use(optimizeCss, [{}]);
         config
-            .plugin("dll")
-            .use(autodllWebpackPlugin, [{
-                inject: true,
-                debug: true,
-                filename: '[name]_[contenthash].js',
-                path: './dll',
-                entry: {
-                    vendor: [
-                        'vue',
-                        'vue-router',
-                        'core-js'
-                    ]
-                }
-            }]);
+            .optimization
+                .minimizer("optimize-css")
+                    .use(optimizeCss, [{}])
         config
             .optimization
                 .splitChunks({
