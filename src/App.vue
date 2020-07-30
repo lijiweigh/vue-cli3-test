39c41e0940e658d14b4b72c36c697cc6d0ec0dbf
jiwei.li <jiwei.li@ly.com>
2020-07-30 19:40:52 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+      <img src="../src/assets/logo.png" alt="">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -15,6 +16,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: url("./assets/logo.png");
 }
 
 #nav {
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 'use strict';
 const path = require('path');
 const fs = require('fs-extra');
+const webpack = require("webpack")
 // const autodllWebpackPlugin = require("autodll-webpack-plugin")
 const optimizeCss = require("optimize-css-assets-webpack-plugin")
 function resolve(dir) {
@@ -9,26 +10,81 @@ function resolve(dir) {
 
 module.exports = {
     publicPath: "./",
-    css: {
-        extract: {
-            publicPath: "//a.b.com/"
+    pages: {
+        "index/index": {
+            entry: "./src/main.js",
+            template: "./public/index.html",
+            filename: "index/index.html"
+        },
+        "copy/copy": {
+            entry: "./src/main.js",
+            template: "./public/index.html",
+            filename: "copy/copy.html"
         }
     },
+    // css: {
+    //     extract: {
+    //         publicPath: "//a.b.com/"
+    //     }
+    // },
 	chainWebpack(config) {
-        config  
-            .entryPoints
-                .clear();
-        config.entry("main/app").add("./src/main.js");
+        config.output
+            .filename(pathdata => {
+                let name = pathdata.chunk.name
+                console.log(name)
+                name = name.split("/")
+                return `${name[0]}/${name[1]}.[contenthash].js`
+            })
+        // config  
+        //     .entryPoints
+        //         .clear();
+        // config.entry("main/app").add("./src/main.js");
         // config
         //     .externals({
         //         "vue": "Vue",
         //         "vue-router": "VueRouter"
         //     })
-		config
-			.plugin('html')
-			.tap(args => {
-				return args
-            });
+        // config.entry("app").prepend("@babel/polyfill")
+        // let oneofs = ["vue-modules", "vue", "normal-modules", "normal"]
+        // oneofs.forEach(key => {
+        //     config.module
+        //         .rule("scss")
+        //             .oneOf(key)
+        //             .use("extract-css-loader")
+        //             .tap(options => {
+        //                 console.log(options)
+        //                 options.publicPath = "https://css.com/"
+        //                 console.log(options)
+        //                 return options
+        //             });
+        // })
+        // config
+        //     .module
+        //         .rule("images")
+        //             .use("url-loader")
+        //                 .tap(options => {
+        //                     return {
+        //                         ...options,
+        //                         publicPath: "https://assets.com/"
+        //                     }
+        //                 })
+        
+        // config
+        //     .plugin("extract-css")
+        //         .tap(args => {
+        //             console.log(args)
+        //             args[0].publicPath = "https://abc.com"
+        //             return args
+        //         })
+
+		// config
+		// 	.plugin('html')
+		// 	.tap(args => {
+		// 		return args
+        //     });
+        config
+            .plugin("hoisting")
+            .use(new webpack.optimize.ModuleConcatenationPlugin, [{}])
         // config
         //     .plugin("dll")
         //     .use(autodllWebpackPlugin, [{
@@ -47,26 +103,26 @@ module.exports = {
         // config
         //     .plugin("optimize-css")
         //         .use(optimizeCss, [{}]);
-        config
-            .optimization
-                .minimizer("optimize-css")
-                    .use(optimizeCss, [{}])
-        config
-            .optimization
-                .splitChunks({
-                    cacheGroups: {
-                        venders: {
-                            test: /[\\/]node_modules[\\/]/,
-                            priority: -10,
-                            chunks: "initial"
-                        },
-                        common: {
-                            minChunks: 1,
-                            priority: -20,
-                            reuseExistingChunk: true
-                        }
-                    }
-                })
+        // config
+        //     .optimization
+        //         .minimizer("optimize-css")
+        //             .use(optimizeCss, [{}])
+        // config
+        //     .optimization
+        //         .splitChunks({
+        //             cacheGroups: {
+        //                 venders: {
+        //                     test: /[\\/]node_modules[\\/]/,
+        //                     priority: -10,
+        //                     chunks: "initial"
+        //                 },
+        //                 common: {
+        //                     minChunks: 1,
+        //                     priority: -20,
+        //                     reuseExistingChunk: true
+        //                 }
+        //             }
+        //         })
         return config
 	}
 };
